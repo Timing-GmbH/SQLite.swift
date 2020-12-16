59fccf79b56f8e3f0f265b92c40b29cc8ea09dc9
Christian Tietze <me@christiantietze.de>
2020-12-16 17:36:58 +0100

    add test to show throwing URL column unwrapping

--- Tests/SQLiteTests/FoundationTests.swift
+++ Tests/SQLiteTests/FoundationTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 import SQLite
 
-class FoundationTests : XCTestCase {
+class FoundationTests : SQLiteTestCase {
     func testDataToBlob() {
         let data = Data([1, 2, 3])
         let blob = data.datatypeValue
@@ -17,5 +17,30 @@ class FoundationTests : XCTestCase {
     func testStringToURL() throws {
         XCTAssertThrowsError(try URL.fromDatatypeValue(""))
         XCTAssertEqual(URL(string: "/")!, try URL.fromDatatypeValue("/"))
+    }
+
+    func testURLColumn() throws {
+        try! db.execute("""
+            CREATE TABLE websites (
+                id INTEGER PRIMARY KEY,
+                url TEXT
+            )
+            """)
+        try db.run("INSERT INTO `websites` (url) VALUES (\"\")")
+        try db.run("INSERT INTO `websites` (url) VALUES (\"https://example.com\")")
+
+        // Primitive values show what's in the DB
+        let websiteURLStrings = try db
+            .prepare("SELECT `url` FROM `websites`")
+            .map { (try! $0.unwrapOrThrow()[0]) as! String  }
+        XCTAssertEqual(websiteURLStrings, ["", "https://example.com"])
+
+        // Types SQLite.swift wrappers show failing URL unwrapping
+        let websites = Table("websites")
+        let url = Expression<URL?>("url")
+        let allWebsites = try Array(try db.prepare(websites)).map { try $0.unwrapOrThrow() }
+        XCTAssertThrowsError(try allWebsites[0].get(url))
+        XCTAssertEqual(URL(string: "https://example.com")!,
+                       try allWebsites[1].get(url))
     }
 }
